2b2150281c9b718a59b74e18480580f072bc87cb
chLi <565353780@qq.com>
2022-09-26 00:07:30 +0800

    add render test

--- scan2cad_dataset_manage/Demo/dataset_loader.py
+++ scan2cad_dataset_manage/Demo/dataset_loader.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 sys.path.append("../mesh-manage")
 
 from scan2cad_dataset_manage.Module.dataset_loader import DatasetLoader
 
-import open3d as o3d
 
 def demo():
     dataset_folder_path = "/home/chli/chLi/Scan2CAD/scan2cad_dataset/"
     scannet_dataset_folder_path = "/home/chli/chLi/ScanNet/scans/"
     shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
     scannet_scene_name = "scene0474_02"
-    scannet_object_file_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/34_monitor.ply"
+    #  scannet_object_file_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/34_monitor.ply"
+    scannet_object_file_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/1_chair.ply"
 
     dataset_loader = DatasetLoader(dataset_folder_path,
                                    scannet_dataset_folder_path,
@@ -24,4 +25,6 @@ def demo():
     print(scannet_object_file_path)
     print("-->")
     print(shapenet_model_file_path)
+
+    dataset_loader.renderScan2CADBBox(scannet_scene_name)
     return True
