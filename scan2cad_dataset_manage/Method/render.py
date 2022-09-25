baf8c052f628a836bc6529802f5580414c65c63c
chLi <565353780@qq.com>
2022-09-26 00:48:36 +0800

    add gt mesh for test

--- scan2cad_dataset_manage/Method/render.py
+++ scan2cad_dataset_manage/Method/render.py
@@ -7,9 +7,22 @@ from scan2cad_dataset_manage.Method.bbox import getOpen3DBBoxFromBBox
 
 
 def renderScan2CADBBox(scene):
-    bbox_list = []
-    for model in scene.model_list:
-        bbox_list.append(getOpen3DBBoxFromBBox(model.bbox))
+    shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
+    gt_mesh_file_path = \
+        "/home/chli/chLi/ScanNet/scans/scene0474_02/scene0474_02_vh_clean.ply"
 
-    o3d.visualization.draw_geometries(bbox_list)
+    bbox_list = []
+    mesh_list = []
+
+    for model in scene.model_list:
+        bbox_list.append(getOpen3DBBoxFromBBox(model.trans_bbox))
+        model_file_path = shapenet_dataset_folder_path + \
+            model.cad_cat_id + "/" + model.cad_id + "/models/model_normalized.obj"
+        mesh = o3d.io.read_triangle_mesh(model_file_path)
+        mesh.transform(model.trans_matrix)
+        mesh_list.append(mesh)
+
+    gt_mesh = o3d.io.read_triangle_mesh(gt_mesh_file_path)
+
+    o3d.visualization.draw_geometries(bbox_list + mesh_list + [gt_mesh])
     return True
