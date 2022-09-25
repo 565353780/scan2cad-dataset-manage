0ba3b31bcd31d4a68b5d3949dd3e46a76ea93b8b
chLi <565353780@qq.com>
2022-09-25 21:19:43 +0800

    simplify dataset code, only save data now

--- scan2cad_dataset_manage/Data/dataset.py
+++ scan2cad_dataset_manage/Data/dataset.py
@@ -1,66 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import json
+from scan2cad_dataset_manage.Data.scene import Scene
 
 
 class Dataset(object):
 
-    def __init__(self,
-                 dataset_folder_path=None,
-                 scannet_dataset_folder_path=None,
-                 shapenet_dataset_folder_path=None):
-        self.dataset_folder_path = None
-        self.scannet_dataset_folder_path = None
-        self.shapenet_dataset_folder_path = None
+    def __init__(self, scene_dict_list=None):
+        self.scene_dict = {}
 
-        self.cad_appearances_json = None
-        self.full_annotations_json = None
-        self.unique_cad_csv = None
-
-        if None not in [
-                dataset_folder_path, scannet_dataset_folder_path,
-                shapenet_dataset_folder_path
-        ]:
-            self.loadDataset(dataset_folder_path, scannet_dataset_folder_path,
-                             shapenet_dataset_folder_path)
+        if scene_dict_list is not None:
+            self.loadSceneDictList(scene_dict_list)
         return
 
-    def reset(self):
-        self.dataset_folder_path = None
-        self.scannet_dataset_folder_path = None
-        self.shapenet_dataset_folder_path = None
-
-        self.cad_appearances_json = None
-        self.full_annotations_json = None
-        self.unique_cad_csv = None
-        return True
-
-    def loadJson(self):
-        cad_appearances_json_file_path = self.dataset_folder_path + "cad_appearances.json"
-        assert os.path.exists(cad_appearances_json_file_path)
-
-        with open(cad_appearances_json_file_path, "r") as f:
-            self.cad_appearances_json = json.load(f)
-
-        full_annotations_json_file_path = self.dataset_folder_path + "full_annotations.json"
-        assert os.path.exists(full_annotations_json_file_path)
-
-        with open(full_annotations_json_file_path, "r") as f:
-            self.full_annotations_json = json.load(f)
-
-        return True
-
-    def loadDataset(self, dataset_folder_path, scannet_dataset_folder_path,
-                    shapenet_dataset_folder_path):
-        assert os.path.exists(dataset_folder_path)
-        assert os.path.exists(scannet_dataset_folder_path)
-        assert os.path.exists(shapenet_dataset_folder_path)
-
-        self.dataset_folder_path = dataset_folder_path
-        self.scannet_dataset_folder_path = scannet_dataset_folder_path
-        self.shapenet_dataset_folder_path = shapenet_dataset_folder_path
-
-        self.loadJson()
+    def loadSceneDictList(self, scene_dict_list):
+        for scene_dict in scene_dict_list:
+            scene_name = scene_dict['id_scan']
+            self.scene_dict[scene_name] = Scene(scene_dict)
         return True
