1ce1b4c5ed2dd0933a1977f65c616633d5921142
chLi <565353780@qq.com>
2022-09-25 21:19:58 +0800

    add scannet_scene_name for test

--- scan2cad_dataset_manage/Demo/dataset_loader.py
+++ scan2cad_dataset_manage/Demo/dataset_loader.py
@@ -8,7 +8,13 @@ def demo():
     dataset_folder_path = "/home/chli/chLi/Scan2CAD/scan2cad_dataset/"
     scannet_dataset_folder_path = "/home/chli/chLi/ScanNet/scans/"
     shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
+
+    scannet_scene_name = "scene0474_02"
+
     dataset_loader = DatasetLoader(dataset_folder_path,
                                    scannet_dataset_folder_path,
                                    shapenet_dataset_folder_path)
+
+    shapenet_model_file_path = dataset_loader.getShapeNetModelFilePath(scannet_scene_name)
+    print(scannet_scene_name, "-->", shapenet_model_file_path)
     return True
