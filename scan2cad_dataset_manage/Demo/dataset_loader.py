4b08ead695d9a909210110d8da1170f956bcc361
chLi <565353780@qq.com>
2022-09-25 21:54:43 +0800

    finish first retrieval test, start show in open3d

--- scan2cad_dataset_manage/Demo/dataset_loader.py
+++ scan2cad_dataset_manage/Demo/dataset_loader.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("../mesh-manage")
+
 from scan2cad_dataset_manage.Module.dataset_loader import DatasetLoader
 
+import open3d as o3d
 
 def demo():
     dataset_folder_path = "/home/chli/chLi/Scan2CAD/scan2cad_dataset/"
     scannet_dataset_folder_path = "/home/chli/chLi/ScanNet/scans/"
     shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
-
     scannet_scene_name = "scene0474_02"
+    scannet_object_file_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/34_monitor.ply"
 
     dataset_loader = DatasetLoader(dataset_folder_path,
                                    scannet_dataset_folder_path,
                                    shapenet_dataset_folder_path)
 
-    shapenet_model_file_path = dataset_loader.getShapeNetModelFilePath(scannet_scene_name)
-    print(scannet_scene_name, "-->", shapenet_model_file_path)
+    shapenet_model_file_path = dataset_loader.getShapeNetModelFilePathByBBoxDist(
+        scannet_scene_name, scannet_object_file_path)
+    print(scannet_object_file_path)
+    print("-->")
+    print(shapenet_model_file_path)
     return True
