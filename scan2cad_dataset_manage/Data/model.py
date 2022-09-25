90af4e08e1d6e54758702c91afcc2f5cd52252dd
chLi <565353780@qq.com>
2022-09-26 00:48:26 +0800

    add trans_matrix and trans_bbox

--- scan2cad_dataset_manage/Data/model.py
+++ scan2cad_dataset_manage/Data/model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 from scan2cad_dataset_manage.Data.trans import Trans
 from scan2cad_dataset_manage.Data.point import Point
 from scan2cad_dataset_manage.Data.bbox import BBox
@@ -17,6 +19,9 @@ class Model(object):
         self.cad_cat_id = None
         self.cad_keypoint_list = None
         self.scan_keypoint_list = None
+
+        self.trans_matrix = None
+        self.trans_bbox = None
 
         if model_dict is not None:
             self.loadModelDict(model_dict)
@@ -58,10 +63,24 @@ class Model(object):
         self.scan_keypoint_list = model_dict['keypoints_scan']['position']
         return True
 
+    def updateTransMatrix(self):
+        self.trans_matrix = self.trans_model_to_world.getTransMatrix()
+
+        bbox_list = self.bbox.toList()
+        bbox_list[0].append(1)
+        bbox_list[1].append(1)
+
+        bbox_array = np.array(bbox_list).transpose(1, 0)
+        trans_bbox_array = np.matmul(self.trans_matrix,
+                                     bbox_array).transpose(1, 0)[:, :3]
+        self.trans_bbox = BBox.fromList(trans_bbox_array)
+        return True
+
     def loadModelDict(self, model_dict):
         assert self.loadTrans(model_dict)
         assert self.loadBBox(model_dict)
         assert self.loadSym(model_dict)
         assert self.loadCAD(model_dict)
         assert self.loadKeyPoints(model_dict)
+        assert self.updateTransMatrix()
         return True
