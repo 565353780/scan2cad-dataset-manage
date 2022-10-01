398affdcb08c003ec5317f250c4d819b53af3f9c
chLi <565353780@qq.com>
2022-10-01 16:20:36 +0800

    now can auto render any scene if valid

--- scan2cad_dataset_manage/Method/render.py
+++ scan2cad_dataset_manage/Method/render.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import open3d as o3d
 
 from scan2cad_dataset_manage.Method.bbox import getOpen3DBBoxFromBBoxArray
 
 
-def renderScan2CADBBox(scene):
+def renderScan2CADBBox(scene, scannet_scene_file_path=None):
     shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
-    gt_mesh_file_path = \
-        "/home/chli/chLi/ScanNet/scans/scene0474_02/scene0474_02_vh_clean.ply"
 
-    gt_mesh = o3d.io.read_triangle_mesh(gt_mesh_file_path)
-
-    bbox_list = []
     mesh_list = []
 
-    for model in scene.model_list:
-        bbox_list.append(getOpen3DBBoxFromBBoxArray(model.trans_bbox_array))
-        model_file_path = shapenet_dataset_folder_path + \
-            model.cad_cat_id + "/" + model.cad_id + "/models/model_normalized.obj"
-        mesh = o3d.io.read_triangle_mesh(model_file_path)
-        mesh.transform(model.trans_model_to_scan_matrix)
-        mesh_list.append(mesh)
+    if scannet_scene_file_path is not None:
+        scannet_scene_mesh = o3d.io.read_triangle_mesh(scannet_scene_file_path)
+        mesh_list.append(scannet_scene_mesh)
 
-    o3d.visualization.draw_geometries(bbox_list + mesh_list + [gt_mesh])
+    bbox_list = []
+
+    for shapenet_model in scene.model_list:
+        bbox_list.append(getOpen3DBBoxFromBBoxArray(shapenet_model.trans_bbox_array))
+        shapenet_model_file_path = shapenet_dataset_folder_path + \
+            shapenet_model.cad_cat_id + "/" + shapenet_model.cad_id + "/models/model_normalized.obj"
+        assert os.path.exists(shapenet_model_file_path)
+        shapenet_model_mesh = o3d.io.read_triangle_mesh(shapenet_model_file_path)
+        shapenet_model_mesh.transform(shapenet_model.trans_model_to_scan_matrix)
+        mesh_list.append(shapenet_model_mesh)
+
+    o3d.visualization.draw_geometries(bbox_list + mesh_list)
     return True
