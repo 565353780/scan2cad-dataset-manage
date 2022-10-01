d26245986c1de34f9372aceb7e44372aaf414816
chLi <565353780@qq.com>
2022-10-01 17:12:24 +0800

    add new demo func into demo script

--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@
 
 from scan2cad_dataset_manage.Demo.dataset_loader import demo as demo_load_dataset
 from scan2cad_dataset_manage.Demo.object_model_map_generator import demo as demo_generate_object_model_map
+from scan2cad_dataset_manage.Demo.object_model_map_manager import demo as demo_manage_object_model_map
 
 if __name__ == "__main__":
     #  demo_load_dataset()
-    demo_generate_object_model_map()
+    #  demo_generate_object_model_map()
+    demo_manage_object_model_map()
