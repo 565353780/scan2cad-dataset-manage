42476c1256d0a1185e9bb8569a3a623b4e5fcc00
chLi <565353780@qq.com>
2022-10-16 22:27:21 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,5 +3,5 @@ git clone git@github.com:565353780/mesh-manage.git
 cd mesh-manage
 ./dev_setup.sh
 
-pip install numpy open3d tqdm
+pip install numpy open3d quaternion tqdm
 
--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ git clone https://github.com/565353780/mesh-manage.git
 cd mesh-manage
 ./setup.sh
 
-pip install numpy open3d tqdm
+pip install numpy open3d quaternion tqdm
 
