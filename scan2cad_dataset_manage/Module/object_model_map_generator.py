e3f0366f94176d16228108ae0b03ce6784b85b9a
chLi <565353780@qq.com>
2022-12-13 15:58:43 +0800

    [FIXME] simplify code, add tag for fixing dataset bug

--- scan2cad_dataset_manage/Module/object_model_map_generator.py
+++ scan2cad_dataset_manage/Module/object_model_map_generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import json
+import os
+
 import numpy as np
+import open3d as o3d
 from tqdm import tqdm
 
 from scan2cad_dataset_manage.Method.path import createFileFolder, renameFile
@@ -79,47 +81,34 @@ class ObjectModelMapGenerator(object):
         scannet_object_center_dict = {}
         for scannet_object_file_name, scannet_object_bbox in scannet_scene_bbox_json.items(
         ):
-            scannet_object_center = [
-                (scannet_object_bbox[0][i] + scannet_object_bbox[1][i]) / 2.0
-                for i in range(3)
-            ]
+            scannet_object_center = np.mean(scannet_object_bbox, axis=0)
+
             scannet_object_center_dict[
                 scannet_object_file_name] = scannet_object_center
 
         scene_object_model_map_dict = {}
 
+        # FIXME: have error in matching scannet models!
         for shapenet_model in scene.model_list:
             model_file_path = self.shapenet_dataset_folder_path + \
                 shapenet_model.cad_cat_id + "/" + shapenet_model.cad_id + "/models/model_normalized.obj"
             assert os.path.exists(model_file_path)
 
-            shapenet_model_min_point_list = [
-                np.min(shapenet_model.trans_bbox_array[:, i]) for i in range(3)
-            ]
-            shapenet_model_max_point_list = [
-                np.max(shapenet_model.trans_bbox_array[:, i]) for i in range(3)
-            ]
-
-            shapenet_model_trans_center = [
-                (shapenet_model_min_point_list[i] +
-                 shapenet_model_max_point_list[i]) / 2.0 for i in range(3)
-            ]
+            shapenet_model_trans_center = np.mean(
+                shapenet_model.trans_bbox_array, axis=0)
 
             min_dist_scannet_object_file_name = None
             min_dist_to_shapenet_model_trans_center = float('inf')
             for scannet_object_file_name, scannet_object_center in scannet_object_center_dict.items(
             ):
-                x_diff = scannet_object_center[
-                    0] - shapenet_model_trans_center[0]
-                y_diff = scannet_object_center[
-                    1] - shapenet_model_trans_center[1]
-                z_diff = scannet_object_center[
-                    2] - shapenet_model_trans_center[2]
-                current_dist_to_shapenet_model_trans_center = x_diff * x_diff + y_diff * y_diff + z_diff * z_diff
+                diff = scannet_object_center - shapenet_model_trans_center
+                current_dist_to_shapenet_model_trans_center = np.linalg.norm(
+                    diff)
                 if current_dist_to_shapenet_model_trans_center < min_dist_to_shapenet_model_trans_center:
                     min_dist_scannet_object_file_name = scannet_object_file_name
                     min_dist_to_shapenet_model_trans_center = current_dist_to_shapenet_model_trans_center
 
+            print(min_dist_scannet_object_file_name)
             scene_object_model_map_dict[
                 min_dist_scannet_object_file_name] = shapenet_model.toDict()
 
@@ -141,14 +130,18 @@ class ObjectModelMapGenerator(object):
             print("\t start generate object model map json for all scenes...")
             for_data = tqdm(for_data)
         for scene_name, scene in for_data:
+            if scene_name != "scene0474_02":
+                continue
             save_map_json_file_path = save_map_json_folder_path + \
                 scene_name + "/object_model_map.json"
 
-            if os.path.exists(save_map_json_file_path):
-                continue
+            #  if os.path.exists(save_map_json_file_path):
+            #  continue
 
             tmp_save_map_json_file_path = save_map_json_file_path[:-5] + "_tmp.json"
-            self.generateSceneObjectModelMap(scene, tmp_save_map_json_file_path)
+            self.generateSceneObjectModelMap(scene,
+                                             tmp_save_map_json_file_path)
 
             renameFile(tmp_save_map_json_file_path, save_map_json_file_path)
+            exit()
         return True
